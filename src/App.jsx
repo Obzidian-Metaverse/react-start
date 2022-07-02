b36737213a62f0f68e74a5e9f75c114f9feb1bb4
fabaps <thecryptonodebnb@gmail.com>
2022-07-02 16:36:55 -0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 import Texto from "./Texto";
 
 function App() {
   if ("ethereum" in window) {
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    console.log(provider);
+    const signer = provider.getSigner();
+
+    async function getWallet() {
+      const wallet = await signer.getAddress();
+      console.log(wallet);
+    }
+
+    getWallet()
+
 
   } else {
     alert("Instala Metamask");
